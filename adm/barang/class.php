83f0f5c2ef9b6893b069cf2c01c67b975cd186b9
winardiaris <ariswinardy@gmail.com>
2014-11-02 19:37:02 +0700

    simabes daily build

--- adm/barang/class.php
+++ adm/barang/class.php
@@ -10,7 +10,7 @@ class barang{
 		elseif(!empty($id_brg) && empty($id_sup)){$qry = "SELECT * FROM br_data WHERE id_brg='$id_brg' ORDER BY id_brg ASC";}
 		else{$qry = "SELECT * FROM br_data WHERE id_brg='$id_brg' AND id_sup='$id_sup' ORDER BY id_brg ASC";};
 
-		$run =mysql_query($qry) ;
+		$run =mysql_query($qry) or die(mysql_error());
 		while($row = mysql_fetch_array($run))
 			$data[] = $row;
 			return $data;
@@ -57,19 +57,19 @@ class barang{
 	}
 	function simpan_barang($id_brg,$kode_brg,$nm_brg,$id_kt_brg,$id_kualitas,$hrg_beli,$hrg_jual,$id_satuan,$stok,$stok_min,$id_rak,$id_sup,$tgl_masuk,$ket_brg,$photo_brg,$wkt_ubah){
 		mysql_query("INSERT INTO br_data (id_brg,kode_brg,nm_brg,id_kt_brg,id_kualitas,hrg_beli,hrg_jual,id_satuan,stok,stok_min,id_rak,id_sup,tgl_masuk,ket_brg,photo_brg,wkt_ubah)
-		VALUES ('$id_brg','$kode_brg','$nm_brg','$id_kt_brg','$id_kualitas','$hrg_beli','$hrg_jual','$id_satuan','$stok','$stok_min','$id_rak','$id_sup','$tgl_masuk','$ket_brg','$photo_brg','$wkt_ubah')") ;
+		VALUES ('$id_brg','$kode_brg','$nm_brg','$id_kt_brg','$id_kualitas','$hrg_beli','$hrg_jual','$id_satuan','$stok','$stok_min','$id_rak','$id_sup','$tgl_masuk','$ket_brg','$photo_brg','$wkt_ubah')") or die(mysql_error());
 	}
 	function cek_ada($id_brg,$kode_brg){
-		$qry = mysql_query("select id_brg,kode_brg FROM br_data WHERE id_brg='$id_brg' OR kode_brg='$kode_brg'") ;
+		$qry = mysql_query("select id_brg,kode_brg FROM br_data WHERE id_brg='$id_brg' OR kode_brg='$kode_brg'") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}
 	function hapus_barang($id_brg){
-		mysql_query("DELETE FROM br_data WHERE id_brg='$id_brg' LIMIT 1") ;
+		mysql_query("DELETE FROM br_data WHERE id_brg='$id_brg' LIMIT 1") or die(mysql_error());
 	}
 	function perbaharui_photo($id_brg,$photo_brg){
-		mysql_query("UPDATE br_data SET photo_brg='$photo_brg' WHERE id_brg='$id_brg'") ;
+		mysql_query("UPDATE br_data SET photo_brg='$photo_brg' WHERE id_brg='$id_brg'") or die (mysql_error());
 	}
 	function perbaharui_barang($id_brg,$kode_brg,$nm_brg,$id_kt_brg,$id_kualitas,$hrg_beli,$hrg_jual,$id_satuan,$stok,$stok_min,$id_rak,$id_sup,$tgl_masuk,$ket_brg,$wkt_ubah){
 		mysql_query("UPDATE br_data SET 
@@ -78,7 +78,7 @@ class barang{
 					id_satuan='$id_satuan',stok='$stok',stok_min='$stok_min',id_rak='$id_rak',
 					id_sup='$id_sup',tgl_masuk='$tgl_masuk',ket_brg='$ket_brg',
 					wkt_ubah='$wkt_ubah' 
-					WHERE id_brg='$id_brg' LIMIT 1") ;
+					WHERE id_brg='$id_brg' LIMIT 1") or die(mysql_error());
 		
 	}
 	function perbaharui_status_barang($id_brg,$dipesan,$stok){
@@ -88,25 +88,25 @@ class barang{
 		else{
 			$qry = "UPDATE br_data SET dipesan='$dipesan',stok='$stok' WHERE id_brg='$id_brg'";
 		}
-		mysql_query($qry) ;
+		mysql_query($qry) or die (mysql_error());
 	}
 	//barang per kendaraan
 	function tampil_br_kendaraan($id_brg){		
-		$qry = mysql_query("SELECT * FROM br_data_perkendaraan WHERE id_brg='$id_brg'") ;
+		$qry = mysql_query("SELECT * FROM br_data_perkendaraan WHERE id_brg='$id_brg'") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}
 	function simpan_brg_kendaraan($id_brg,$id_kendaraan,$wkt_ubah){
-		mysql_query("INSERT INTO br_data_perkendaraan (id_brg,id_kendaraan,wkt_ubah) values ('$id_brg','$id_kendaraan','$sekarang')") ;
+		mysql_query("INSERT INTO br_data_perkendaraan (id_brg,id_kendaraan,wkt_ubah) values ('$id_brg','$id_kendaraan','$sekarang')") or die(mysql_error());
 	}
 	function hapus_brg_kendaraan($id_brg){
-		mysql_query("DELETE  FROM `br_data_perkendaraan` WHERE id_brg='$id_brg'") ;
+		mysql_query("DELETE  FROM `br_data_perkendaraan` WHERE id_brg='$id_brg'") or die(mysql_error());
 	}
 	
 	//kategori
 	function tampil_kategori(){
-		$qry = mysql_query("SELECT * FROM br_kategori") ;
+		$qry = mysql_query("SELECT * FROM br_kategori") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
@@ -122,24 +122,24 @@ class barang{
 			return $data['nm_kt_brg'];
 	}
 	function simpan_kategori($id_kt_brg,$nm_kt_brg,$wkt_ubah){
-		mysql_query("INSERT INTO br_kategori values ('$id_kt_brg','$nm_kt_brg','$wkt_ubah') ") ;		
+		mysql_query("INSERT INTO br_kategori values ('$id_kt_brg','$nm_kt_brg','$wkt_ubah') ") or die(mysql_error());		
 	}
 	function cek_kategori($id_kt_brg){
-		$qry = mysql_query("SELECT * FROM br_kategori WHERE id_kt_brg='$id_kt_brg'") ;
+		$qry = mysql_query("SELECT * FROM br_kategori WHERE id_kt_brg='$id_kt_brg'") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}
 	function hapus_kategori($id_kt_brg){
-		mysql_query("DELETE FROM br_kategori WHERE id_kt_brg='$id_kt_brg'") ;
+		mysql_query("DELETE FROM br_kategori WHERE id_kt_brg='$id_kt_brg'") or die(mysql_error());
 	}
 	function perbaharui_kategori($id_kt_brg,$nm_kt_brg,$wkt_ubah){
-		mysql_query("UPDATE br_kategori SET nm_kt_brg='$nm_kt_brg',wkt_ubah='$wkt_ubah' WHERE id_kt_brg='$id_kt_brg' ") ;
+		mysql_query("UPDATE br_kategori SET nm_kt_brg='$nm_kt_brg',wkt_ubah='$wkt_ubah' WHERE id_kt_brg='$id_kt_brg' ") or die(mysql_error());
 	}
 	
 	//jenis kendaraan
 	function tampil_jenis_kendaraan(){
-		$qry = mysql_query("SELECT * FROM br_kendaraan") ;
+		$qry = mysql_query("SELECT * FROM br_kendaraan") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
@@ -155,25 +155,25 @@ class barang{
 			return $data['kendaraan'];
 	}
 	function simpan_jenis_kendaraan($id_kendaraan,$kendaraan,$wkt_ubah){
-		mysql_query("INSERT INTO br_kendaraan VALUES('$id_kendaraan','$kendaraan','$wkt_ubah')") ;
+		mysql_query("INSERT INTO br_kendaraan VALUES('$id_kendaraan','$kendaraan','$wkt_ubah')") or die(mysql_error());
 		
 	}
 	function cek_jenis_kendaraan($id_kendaraan){
-		$qry = mysql_query("SELECT * FROM br_kendaraan WHERE id_kendaraan='$id_kendaraan'") ;
+		$qry = mysql_query("SELECT * FROM br_kendaraan WHERE id_kendaraan='$id_kendaraan'") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}
 	function hapus_jenis_kendaraan($id_kendaraan){
-		mysql_query("DELETE FROM br_kendaraan WHERE id_kendaraan='$id_kendaraan' ") ;
+		mysql_query("DELETE FROM br_kendaraan WHERE id_kendaraan='$id_kendaraan' ") or die(mysql_error());
 	}
 	function perbaharui_jenis_kendaraan($id_kendaraan,$kendaraan,$wkt_ubah){
-		mysql_query("UPDATE br_kendaraan SET kendaraan='$kendaraan', wkt_ubah='$wkt_ubah' WHERE id_kendaraan='$id_kendaraan'") ;
+		mysql_query("UPDATE br_kendaraan SET kendaraan='$kendaraan', wkt_ubah='$wkt_ubah' WHERE id_kendaraan='$id_kendaraan'") or die(mysql_error());
 	}
 	
 	//kualitas
 	function tampil_kualitas(){
-		$qry = mysql_query("SELECT * FROM br_kualitas") ;
+		$qry = mysql_query("SELECT * FROM br_kualitas") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
@@ -188,24 +188,24 @@ class barang{
 			return $data['kualitas'];
 	}
 	function simpan_kualitas($id_kualitas,$kualitas,$wkt_ubah){
-		mysql_query("INSERT INTO br_kualitas VALUES('$id_kualitas','$kualitas','$wkt_ubah')") ;		
+		mysql_query("INSERT INTO br_kualitas VALUES('$id_kualitas','$kualitas','$wkt_ubah')") or die(mysql_error());		
 	}
 	function cek_kualitas($id_kualitas){
-		$qry = mysql_query("SELECT * FROM br_kualitas WHERE id_kualitas='$id_kualitas'") ;
+		$qry = mysql_query("SELECT * FROM br_kualitas WHERE id_kualitas='$id_kualitas'") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}
 	function hapus_kualitas($id_kualitas){
-		mysql_query("DELETE FROM br_kualitas WHERE id_kualitas='$id_kualitas'") ;
+		mysql_query("DELETE FROM br_kualitas WHERE id_kualitas='$id_kualitas'") or die(mysql_error());
 	}
 	function perbaharui_kualitas($id_kualitas,$kualitas,$wkt_ubah){
-		mysql_query("UPDATE br_kualitas SET kualitas='$kualitas',wkt_ubah='$wkt_ubah' WHERE id_kualitas='$id_kualitas' ") ;
+		mysql_query("UPDATE br_kualitas SET kualitas='$kualitas',wkt_ubah='$wkt_ubah' WHERE id_kualitas='$id_kualitas' ") or die(mysql_error());
 	}
 	
 	//satuan
 	function tampil_satuan(){
-		$qry = mysql_query("SELECT * FROM br_satuan") ;
+		$qry = mysql_query("SELECT * FROM br_satuan") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
@@ -220,24 +220,24 @@ class barang{
 			return $data['satuan'];
 	}
 	function simpan_satuan($id_satuan,$satuan,$wkt_ubah){
-		mysql_query("INSERT INTO br_satuan VALUES('$id_satuan','$satuan','$wkt_ubah')") ;
+		mysql_query("INSERT INTO br_satuan VALUES('$id_satuan','$satuan','$wkt_ubah')") or die(mysql_error());
 	}
 	function cek_satuan($id_satuan){
-		$qry = mysql_query("SELECT id_satuan FROM br_satuan WHERE id_satuan='$id_satuan'") ;
+		$qry = mysql_query("SELECT id_satuan FROM br_satuan WHERE id_satuan='$id_satuan'") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}
 	function hapus_satuan($id_satuan){
-		mysql_query("DELETE FROM br_satuan WHERE id_satuan='$id_satuan'") ;
+		mysql_query("DELETE FROM br_satuan WHERE id_satuan='$id_satuan'") or die(mysql_error());
 	}
 	function perbaharui_satuan($id_satuan,$satuan,$wkt_ubah){
-		mysql_query("UPDATE br_satuan SET satuan='$satuan', wkt_ubah='$wkt_ubah' WHERE id_satuan='$id_satuan' ") ;
+		mysql_query("UPDATE br_satuan SET satuan='$satuan', wkt_ubah='$wkt_ubah' WHERE id_satuan='$id_satuan' ") or die(mysql_error());
 	}
 	
 	//rak
 	function tampil_rak(){
-		$qry = mysql_query("SELECT * FROM br_rak") ;
+		$qry = mysql_query("SELECT * FROM br_rak") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
@@ -254,19 +254,19 @@ class barang{
 			return $data['ket'];
 	}
 	function simpan_rak($nm_rak,$ket,$wkt_ubah){
-		mysql_query("INSERT INTO br_rak VALUES ('','$nm_rak','$ket','$wkt_ubah')") ;
+		mysql_query("INSERT INTO br_rak VALUES ('','$nm_rak','$ket','$wkt_ubah')") or die(mysql_error());
 	}
 	function cek_rak($nm_rak){
-		$qry = mysql_query("SELECT * FROM br_rak WHERE nm_rak='$nm_rak'") ;
+		$qry = mysql_query("SELECT * FROM br_rak WHERE nm_rak='$nm_rak'") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}
 	function hapus_rak($id_rak){
-		mysql_query("DELETE FROM br_rak WHERE id_rak='$id_rak'") ;
+		mysql_query("DELETE FROM br_rak WHERE id_rak='$id_rak'") or die(mysql_error());
 	}
 	function perbaharui_rak($id_rak,$nm_rak,$ket,$wkt_ubah){
-		mysql_query("UPDATE br_rak SET nm_rak='$nm_rak',ket='$ket',wkt_ubah='$wkt_ubah' WHERE id_rak='$id_rak' ") ;
+		mysql_query("UPDATE br_rak SET nm_rak='$nm_rak',ket='$ket',wkt_ubah='$wkt_ubah' WHERE id_rak='$id_rak' ") or die(mysql_error());
 	}
 	
 	//penyalur
@@ -274,7 +274,7 @@ class barang{
 		if(!empty($_GET['cari'])){$qry = "SELECT * FROM sup_data WHERE id_sup='".$_GET['cari']."'OR nm_sup LIKE '%".$_GET['cari']."%' ORDER BY id_sup ASC";}
 		else{$qry = "SELECT * FROM sup_data";}
 		
-		$run =mysql_query($qry) ;
+		$run =mysql_query($qry) or die(mysql_error());
 		while($row = mysql_fetch_array($run))
 			$data[] = $row;
 			return $data;
@@ -293,24 +293,24 @@ class barang{
 			return $data['telp_sup'];
 	}
 	function simpan_penyalur($nm_sup,$almt_sup,$telp_sup,$wkt_ubah){
-		mysql_query("INSERT INTO sup_data VALUES ('','$nm_sup','$almt_sup','$telp_sup','$wkt_ubah')") ;
+		mysql_query("INSERT INTO sup_data VALUES ('','$nm_sup','$almt_sup','$telp_sup','$wkt_ubah')") or die(mysql_error());
 	}
 	function cek_penyalur($nm_sup){
-		$qry = mysql_query("SELECT * FROM sup_data WHERE nm_sup='$nm_sup'") ;
+		$qry = mysql_query("SELECT * FROM sup_data WHERE nm_sup='$nm_sup'") or die(mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}
 	function hapus_penyalur($id_sup){
-		mysql_query("DELETE FROM sup_data WHERE id_sup='$id_sup'") ;
+		mysql_query("DELETE FROM sup_data WHERE id_sup='$id_sup'") or die(mysql_error());
 	}
 	function perbaharui_penyalur($id_sup,$nm_sup,$almt_sup,$telp_sup,$wkt_ubah){
-		mysql_query("UPDATE sup_data SET nm_sup='$nm_sup',almt_sup='$almt_sup',telp_sup='$telp_sup',wkt_ubah='$wkt_ubah' WHERE id_sup='$id_sup' ") ;
+		mysql_query("UPDATE sup_data SET nm_sup='$nm_sup',almt_sup='$almt_sup',telp_sup='$telp_sup',wkt_ubah='$wkt_ubah' WHERE id_sup='$id_sup' ") or die(mysql_error());
 	}
 	
 	//stok kurang
 	function tampil_stok_kurang(){
-		$qry = mysql_query("SELECT * FROM br_data  WHERE stok<=stok_min  AND dipesan='0' ORDER BY id_sup ASC") ;
+		$qry = mysql_query("SELECT * FROM br_data  WHERE stok<=stok_min  AND dipesan='0' ORDER BY id_sup ASC") or die (mysql_error());
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
@@ -327,7 +327,7 @@ class barang{
 		else{
 			$qry = "SELECT * FROM br_pembelian_detail WHERE no_pes='$no_pes' AND id_sup='$id_sup' ";
 		}
-		$hasil = mysql_query($qry)  ;
+		$hasil = mysql_query($qry)  or die (mysql_error());
 		while($row = mysql_fetch_array($hasil))
 			$data[] = $row;
 			return $data;
@@ -343,16 +343,16 @@ class barang{
 		elseif(!empty($no_pes) AND empty($id_brg) AND !empty($diterima)){
 			$qry="SELECT SUM( total ) AS total, id_brg, id_sup FROM  br_pembelian_detail WHERE no_pes='$no_pes' AND diterima='$diterima' ";
 		}
-		$hasil = mysql_query($qry) ;
+		$hasil = mysql_query($qry) or die(mysql_error());
 		while($row = mysql_fetch_array($hasil))
 			$data[] = $row;
 			return $data;
 	}
 	function simpan_pembelian_detail($no_pes,$tgl_pes,$id_sup,$id_brg,$hrg_brg,$jml_brg,$total,$wkt_ubah){
-		mysql_query("INSERT INTO br_pembelian_detail VALUES('','$no_pes','$tgl_pes','$id_sup','$id_brg','$hrg_brg','$jml_brg','$total','0','$wkt_ubah')") ;
+		mysql_query("INSERT INTO br_pembelian_detail VALUES('','$no_pes','$tgl_pes','$id_sup','$id_brg','$hrg_brg','$jml_brg','$total','0','$wkt_ubah')") or die(mysql_error());
 	}
 	function perbaharui_pembelian_detail($no_pes,$id_brg){
-		mysql_query("UPDATE br_pembelian_detail SET diterima='1' WHERE no_pes='$no_pes' AND id_brg='$id_brg'") ;
+		mysql_query("UPDATE br_pembelian_detail SET diterima='1' WHERE no_pes='$no_pes' AND id_brg='$id_brg'") or die(mysql_error());
 	}
 	function tampil_pembelian(){
 		if(!empty($_GET['cari'])){
@@ -361,7 +361,7 @@ class barang{
 		else{
 			$qry = "SELECT * FROM `br_pembelian` ORDER by wkt_ubah DESC";
 		}
-		$hasil = mysql_query($qry)  ;
+		$hasil = mysql_query($qry)  or die (mysql_error());
 		while($row = mysql_fetch_array($hasil))
 			$data[] = $row;
 			return $data;
@@ -384,16 +384,16 @@ class barang{
 			return $data['id_pengguna'];
 	}
 	function simpan_pembelian($no_pes,$tgl_pes,$id_sup,$total_pembayaran,$diterima,$id_pengguna,$wkt_ubah){
-		mysql_query("INSERT INTO br_pembelian VALUES('$no_pes','$tgl_pes','$id_sup','$total_pembayaran','$diterima','$id_pengguna','$wkt_ubah')") ;
+		mysql_query("INSERT INTO br_pembelian VALUES('$no_pes','$tgl_pes','$id_sup','$total_pembayaran','$diterima','$id_pengguna','$wkt_ubah')") or die(mysql_error());
 	}
 	function perbaharui_pembelian($no_pes,$total_pembayaran){
-		mysql_query("UPDATE br_pembelian SET total_pembayaran='$total_pembayaran', diterima='1' WHERE no_pes='$no_pes' ") ;
+		mysql_query("UPDATE br_pembelian SET total_pembayaran='$total_pembayaran', diterima='1' WHERE no_pes='$no_pes' ") or die(mysql_error());
 	}
 	
 	
 	//keuangan
 	function simpan_keuangan($tgl,$ket,$masuk,$keluar){
-		mysql_query("INSERT INTO keuangan VALUES('','$tgl','$ket','$masuk','$keluar')") ;
+		mysql_query("INSERT INTO keuangan VALUES('','$tgl','$ket','$masuk','$keluar')") or die (mysql_error());
 	}
 	
 	
@@ -408,23 +408,23 @@ class barang{
 		else{
 			$qry= "SELECT * FROM sementara";
 		}
-		$hasil = mysql_query($qry) ;
+		$hasil = mysql_query($qry) or die(mysql_error());
 		while($row = mysql_fetch_array($hasil))
 			$data[] = $row;
 			return $data;
 	}
 	function tambah_sementara($id_sementara,$value){
-		mysql_query("INSERT INTO sementara VALUES ('$id_sementara','$value')") ;
+		mysql_query("INSERT INTO sementara VALUES ('$id_sementara','$value')") or die(mysql_error());
 	}
 	function hapus_sementara($id_sementara,$value){
-		mysql_query("DELETE FROM sementara WHERE id_sementara='$id_sementara' AND value='$value'") ;
+		mysql_query("DELETE FROM sementara WHERE id_sementara='$id_sementara' AND value='$value'") or die(mysql_error());
 	}
 	
 	
 	//catatan sistem
 	function log($log_tipe,$pengguna,$log_lokasi,$log_pesan,$log_waktu){
 		$qry=" INSERT INTO log_sistem VALUES ('','$log_tipe','$pengguna','$log_lokasi','$log_pesan','$log_waktu')";
-		mysql_query($qry) ;
+		mysql_query($qry) or die (mysql_error());
 	}
 }
 
