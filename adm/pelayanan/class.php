bb5cf3ec16a9e7d2ca5cdb614d34a48d7e30445e
winardiaris <ariswinardy@gmail.com>
2014-11-02 15:23:34 +0700

    simabes daily build

--- adm/pelayanan/class.php
+++ adm/pelayanan/class.php
@@ -5,29 +5,29 @@ class pelayanan{
 		mysql_query("INSERT INTO ply_wo 
 		(no_wo,id_plg,tgl_wo,no_polisi,no_mesin,jns_kendaraan,km_terakhir,keluhan,wkt_ubah) 
 		VALUES
-		('$no_wo','$id_plg','$tgl_wo','$no_polisi','$no_mesin','$jns_kendaraan','$km_terakhir','$keluhan','$wkt_ubah')") or de(mysql_error());
+		('$no_wo','$id_plg','$tgl_wo','$no_polisi','$no_mesin','$jns_kendaraan','$km_terakhir','$keluhan','$wkt_ubah')") ;
 	}
 	function simpan_wo_dua($no_struk,$no_wo,$tgl_struk,$id_peg,$saran,$wkt_ubah){
-		mysql_query("INSERT INTO ply_ (no_struk,no_wo,tgl_struk,wkt_ubah) VALUES ('$no_struk','$no_wo','$tgl_struk','$wkt_ubah')") or de(mysql_error());
-		mysql_query("UPDATE ply_wo SET saran='$saran', id_peg='$id_peg', status='1', wkt_ubah='$wkt_ubah' WHERE no_wo='$no_wo'") or de(mysql_error());
+		mysql_query("INSERT INTO ply_ (no_struk,no_wo,tgl_struk,wkt_ubah) VALUES ('$no_struk','$no_wo','$tgl_struk','$wkt_ubah')") ;
+		mysql_query("UPDATE ply_wo SET saran='$saran', id_peg='$id_peg', status='1', wkt_ubah='$wkt_ubah' WHERE no_wo='$no_wo'") ;
 	}
 	function perbaharui_wo($no_struk,$no_wo,$status,$saran,$wkt_ubah){
-		mysql_query("UPDATE ply_ SET wkt_ubah='$wkt_ubah' WHERE no_struk='$no_struk' AND no_wo='$no_wo'") or die(mysql_error());
-		mysql_query("UPDATE ply_wo SET status='$status', saran='$saran', wkt_ubah='$wkt_ubah' WHERE no_wo='$no_wo'")or die(mysql_error());
+		mysql_query("UPDATE ply_ SET wkt_ubah='$wkt_ubah' WHERE no_struk='$no_struk' AND no_wo='$no_wo'") ;
+		mysql_query("UPDATE ply_wo SET status='$status', saran='$saran', wkt_ubah='$wkt_ubah' WHERE no_wo='$no_wo'");
 	}
 	function simpan_ply($no_struk,$no_wo,$tot_bayar,$id_pengguna,$wkt_ubah){
 		mysql_query("UPDATE ply_ SET total_pembayaran='$tot_bayar', id_pengguna='$id_pengguna', wkt_ubah='$wkt_ubah' WHERE no_struk='$no_struk' AND no_wo='$no_wo'");	
 	}
 	function selesai_ply($no_struk,$uang_bayar,$wkt_ubah){
-		mysql_query("UPDATE ply_ SET uang_bayar='$uang_bayar', wkt_ubah='$wkt_ubah' WHERE no_struk='$no_struk' ") or die (mysql_error());
+		mysql_query("UPDATE ply_ SET uang_bayar='$uang_bayar', wkt_ubah='$wkt_ubah' WHERE no_struk='$no_struk' ") ;
 	}
 	
 	//penjualan
 	function simpan_pl($no_struk,$tgl_trans,$nm_plg,$tot_bayar,$id_pengguna,$wkt_ubah){
-		mysql_query("INSERT INTO ply_penjualan VALUES ('$no_struk','$tgl_trans','$nm_plg','$tot_bayar','0','$id_pengguna','$wkt_ubah')") or die(mysql_error());
+		mysql_query("INSERT INTO ply_penjualan VALUES ('$no_struk','$tgl_trans','$nm_plg','$tot_bayar','0','$id_pengguna','$wkt_ubah')") ;
 	}
 	function selesai_pl($no_struk,$uang_bayar){
-		mysql_query("UPDATE ply_penjualan SET `uang_bayar`='$uang_bayar' WHERE `no_struk`='$no_struk' ") or die(mysql_error());
+		mysql_query("UPDATE ply_penjualan SET `uang_bayar`='$uang_bayar' WHERE `no_struk`='$no_struk' ") ;
 	}
 	
 	function tampil_wo(){
@@ -36,14 +36,14 @@ class pelayanan{
 		elseif(!empty($_GET['cari'])){$cari = $_GET['cari'];$qry = "SELECT * FROM ply_wo WHERE no_wo='".$cari."' OR id_plg='".$cari."'	OR no_polisi='".$cari."' OR id_peg='".$cari."' ORDER BY no_wo DESC";}
 		else{$qry = "SELECT * FROM ply_wo ORDER BY no_wo DESC";}
 		
-		$run = mysql_query($qry) or die (mysql_error());
+		$run = mysql_query($qry) ;
 		while($row = mysql_fetch_array($run))
 			$data[] = $row;
 			return $data;
 	}
 	function tampil_wo2($field,$no_wo){
 		$qry = 	mysql_query("SELECT  ply_wo.id_plg, dt_pelanggan.nm_plg, ply_wo.jns_kendaraan, ply_wo.no_polisi, ply_wo.no_mesin, ply_wo.keluhan, ply_wo.id_peg, ply_wo.saran FROM ply_wo 
-				INNER JOIN dt_pelanggan ON ply_wo.id_plg = dt_pelanggan.id_plg WHERE ply_wo.no_wo = '$no_wo' LIMIT 1") or die (mysql_error());
+				INNER JOIN dt_pelanggan ON ply_wo.id_plg = dt_pelanggan.id_plg WHERE ply_wo.no_wo = '$no_wo' LIMIT 1") ;
 		$data = mysql_fetch_array($qry);
 		if($field == 'id_plg')
 			return $data['id_plg'];
@@ -82,7 +82,7 @@ class pelayanan{
 					WHERE total_pembayaran>0
 					ORDER BY ply_.no_struk DESC";
 		}
-		$run = mysql_query($qry) or die (mysql_error());
+		$run = mysql_query($qry) ;
 		while($row = mysql_fetch_array($run))
 			$data[] = $row;
 			return $data;
@@ -90,7 +90,7 @@ class pelayanan{
 	function tampil_sejarah_pl(){
 		if(!empty($_GET['cari'])){$cari = $_GET['cari'];$qry ="SELECT * FROM ply_penjualan WHERE no_struk='".$cari."' OR nm_plg LIKE '%".$cari."%'  ORDER BY no_struk DESC";}
 		else{$qry="	SELECT * FROM ply_penjualan ORDER BY no_struk DESC";}
-		$run = mysql_query($qry) or die (mysql_error());
+		$run = mysql_query($qry) ;
 		while($row = mysql_fetch_array($run))
 			$data[] = $row;
 			return $data;
@@ -98,7 +98,7 @@ class pelayanan{
 
 	//
 	function cek_stok($id_brg){
-		$qry = mysql_query("SELECT `stok` FROM `br_data` WHERE `id_brg`='$id_brg' LIMIT 1") or die(mysql_error());
+		$qry = mysql_query("SELECT `stok` FROM `br_data` WHERE `id_brg`='$id_brg' LIMIT 1") ;
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
@@ -110,46 +110,46 @@ class pelayanan{
 			return $data;
 	}
 	function cek_pelayanan($no_struk,$id_kt_ply){
-		$qry=mysql_query("SELECT `id_kt_ply`  FROM  `ply_detail` WHERE `no_struk`='$no_struk' AND `id_kt_ply` LIKE '%$id_kt_ply%' ") or die(mysql_error());
+		$qry=mysql_query("SELECT `id_kt_ply`  FROM  `ply_detail` WHERE `no_struk`='$no_struk' AND `id_kt_ply` LIKE '%$id_kt_ply%' ") ;
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}		
 	function tambah_barang($no_struk,$id_brg,$jml_brg,$wkt_ubah){
-		$qry = mysql_query("SELECT hrg_jual FROM br_data WHERE id_brg='$id_brg' ") or die (mysql_error());
+		$qry = mysql_query("SELECT hrg_jual FROM br_data WHERE id_brg='$id_brg' ") ;
 		$data = mysql_fetch_object($qry);
 		$total= $data->hrg_jual * $jml_brg;
 		mysql_query("INSERT INTO ply_penjualan_detail VALUES ('$no_struk','$id_brg','$jml_brg','$total','$wkt_ubah')");
 	}
 	function perbaharui_barang($no_struk,$id_brg,$sudah_ada,$jml_brg,$wkt_ubah){
-		$qry = mysql_query("SELECT hrg_jual FROM br_data WHERE id_brg='$id_brg' ") or die (mysql_error());
+		$qry = mysql_query("SELECT hrg_jual FROM br_data WHERE id_brg='$id_brg' ") ;
 		$data = mysql_fetch_object($qry);
 		
 		$tambah = $sudah_ada + $jml_brg;
 		$total= $data->hrg_jual * $tambah;
 		
-		mysql_query("UPDATE ply_penjualan_detail SET jml_brg='$tambah',total='$total' WHERE no_struk='$no_struk' AND id_brg='$id_brg'") or die(mysql_error());
+		mysql_query("UPDATE ply_penjualan_detail SET jml_brg='$tambah',total='$total' WHERE no_struk='$no_struk' AND id_brg='$id_brg'") ;
 	}
 	function perbaharui_status_barang($id_brg,$jml_brg){
-		$qry = mysql_query("SELECT stok,terjual FROM br_data WHERE id_brg='$id_brg' ") or die (mysql_error());
+		$qry = mysql_query("SELECT stok,terjual FROM br_data WHERE id_brg='$id_brg' ") ;
 		$data = mysql_fetch_object($qry);
 		$stok=$data->stok - $jml_brg;
 		$terjual=$data->terjual + $jml_brg;	
 			
-		mysql_query("UPDATE br_data SET stok='$stok', terjual='$terjual' WHERE id_brg='$id_brg'") or die(mysql_error());
+		mysql_query("UPDATE br_data SET stok='$stok', terjual='$terjual' WHERE id_brg='$id_brg'") ;
 	}
 	function tambah_pelayanan($no_struk,$id_kt_ply,$wkt_ubah){
-		mysql_query("INSERT INTO ply_detail VALUES ('$no_struk','$id_kt_ply','$wkt_ubah')") or die(mysql_error());
+		mysql_query("INSERT INTO ply_detail VALUES ('$no_struk','$id_kt_ply','$wkt_ubah')") ;
 	}
 	
 	function ambil_no_struk($field,$no_wo){
-		$qry = mysql_query("SELECT no_struk FROM ply_ WHERE no_wo='$no_wo'")or die(mysql_error());
+		$qry = mysql_query("SELECT no_struk FROM ply_ WHERE no_wo='$no_wo'");
 		$data = mysql_fetch_array($qry);
 		if($field == 'no_struk')
 			return $data['no_struk'];
 	}
 	function ambil_pelanggan(){
-		$qry=	mysql_query("SELECT * FROM dt_pelanggan WHERE masa_berlaku >  tgl_registrasi ORDER by id_plg ASC ")or die (mysql_error());
+		$qry=	mysql_query("SELECT * FROM dt_pelanggan WHERE masa_berlaku >  tgl_registrasi ORDER by id_plg ASC ");
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
@@ -157,29 +157,29 @@ class pelayanan{
 	
 		
 	function ambil_pegawai(){
-		$q = mysql_query("SELECT kel_id FROM kel_pengguna WHERE nm_kel LIKE '%mekanik%'") or die(mysql_error());
+		$q = mysql_query("SELECT kel_id FROM kel_pengguna WHERE nm_kel LIKE '%mekanik%'") ;
 		$qq = mysql_fetch_object($q);
 		$kel_id = $qq->kel_id;
 					
-		$qry=	mysql_query("SELECT * FROM dt_pegawai WHERE kel_id='$kel_id' ORDER by id_peg ASC ")or die (mysql_error());
+		$qry=	mysql_query("SELECT * FROM dt_pegawai WHERE kel_id='$kel_id' ORDER by id_peg ASC ");
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}
 	function ambil_brg(){
-		$qry=	mysql_query("SELECT * FROM br_data")or die (mysql_error());
+		$qry=	mysql_query("SELECT * FROM br_data");
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}	
 	function ambil_ply_detail($no_struk){
-		$qry = mysql_query("SELECT ply_detail.id_kt_ply, ply_kategori.nm_kt_ply, ply_kategori.biaya FROM ply_kategori INNER JOIN ply_detail ON ply_kategori.id_kt_ply = ply_detail.id_kt_ply WHERE ply_detail.no_struk = '$no_struk'") or die(mysql_error());
+		$qry = mysql_query("SELECT ply_detail.id_kt_ply, ply_kategori.nm_kt_ply, ply_kategori.biaya FROM ply_kategori INNER JOIN ply_detail ON ply_kategori.id_kt_ply = ply_detail.id_kt_ply WHERE ply_detail.no_struk = '$no_struk'") ;
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}
 	function ambil_ply_penjualan($no_struk){
-		$qry = mysql_query("SELECT ply_penjualan_detail.id_brg, br_data.nm_brg, br_data.hrg_jual, ply_penjualan_detail.jml_brg, ply_penjualan_detail.total FROM br_data INNER JOIN ply_penjualan_detail ON br_data.id_brg = ply_penjualan_detail.id_brg WHERE ply_penjualan_detail.no_struk='$no_struk' ORDER BY id_brg asc ") or die(mysql_error());
+		$qry = mysql_query("SELECT ply_penjualan_detail.id_brg, br_data.nm_brg, br_data.hrg_jual, ply_penjualan_detail.jml_brg, ply_penjualan_detail.total FROM br_data INNER JOIN ply_penjualan_detail ON br_data.id_brg = ply_penjualan_detail.id_brg WHERE ply_penjualan_detail.no_struk='$no_struk' ORDER BY id_brg asc ") ;
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
@@ -206,16 +206,16 @@ class pelayanan{
 	
 	//kategori
 	function ambil_kt_pelayanan(){
-		$qry=	mysql_query("SELECT * FROM ply_kategori")or die (mysql_error());
+		$qry=	mysql_query("SELECT * FROM ply_kategori");
 		while($row = mysql_fetch_array($qry))
 			$data[] = $row;
 			return $data;
 	}
 	function simpan_kt_ply($nm_kt_ply,$biaya,$wkt_ubah){
-		mysql_query("INSERT INTO ply_kategori (nm_kt_ply,biaya,wkt_ubah)VALUES ('$nm_kt_ply','$biaya','$wkt_ubah')") or die (mysql_error());
+		mysql_query("INSERT INTO ply_kategori (nm_kt_ply,biaya,wkt_ubah)VALUES ('$nm_kt_ply','$biaya','$wkt_ubah')") ;
 	}		
 	function hapus_kt_ply($id_kt_ply){
-		mysql_query("DELETE FROM ply_kategori WHERE id_kt_ply='$id_kt_ply' LIMIT 1") or die(mysql_error());
+		mysql_query("DELETE FROM ply_kategori WHERE id_kt_ply='$id_kt_ply' LIMIT 1") ;
 	}
 	function sunting_kt_ply($field,$id_kt_ply){
 		$qry = "SELECT * FROM ply_kategori WHERE id_kt_ply='$id_kt_ply' LIMIT 1";
@@ -229,7 +229,7 @@ class pelayanan{
 			return $data['biaya'];
 	}
 	function perbaharui_kt_ply($id_kt_ply,$nm_kt_ply,$biaya,$wkt_ubah){
-		mysql_query("UPDATE  ply_kategori SET nm_kt_ply='$nm_kt_ply',biaya='$biaya',wkt_ubah='$wkt_ubah' WHERE id_kt_ply='$id_kt_ply' ") or die(mysql_error());
+		mysql_query("UPDATE  ply_kategori SET nm_kt_ply='$nm_kt_ply',biaya='$biaya',wkt_ubah='$wkt_ubah' WHERE id_kt_ply='$id_kt_ply' ") ;
 	}
 	
 	//
@@ -241,19 +241,19 @@ class pelayanan{
 			return $data['transaksi'];
 	}
 	function perbaharui_transaksi_plg($id_plg,$transaksi){
-		mysql_query("UPDATE dt_pelanggan SET transaksi='$transaksi' WHERE id_plg='$id_plg'") or die(mysql_error());
+		mysql_query("UPDATE dt_pelanggan SET transaksi='$transaksi' WHERE id_plg='$id_plg'") ;
 	}
 	
 	//pelaporan keuangan
 	function tambah_pelaporan_keuangan($tgl,$ket,$masuk,$keluar){
-		mysql_query(" INSERT INTO keuangan VALUES('','$tgl','$ket','$masuk','keluar')") or die(mysql_error());
+		mysql_query(" INSERT INTO keuangan VALUES('','$tgl','$ket','$masuk','keluar')") ;
 	}
 	
 	
 	//catatan sistem
 	function log($log_tipe,$pengguna,$log_lokasi,$log_pesan,$log_waktu){
 		$qry=" INSERT INTO log_sistem VALUES ('','$log_tipe','$pengguna','$log_lokasi','$log_pesan','$log_waktu')";
-		mysql_query($qry) or die (mysql_error());
+		mysql_query($qry) ;
 	}
 }
 ?>
